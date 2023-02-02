ebe9488b94754399da15923eeb24c65f8fc87816
Nydwhal <cyril.cauquil@gmail.com>
2023-02-02 09:42:10 +0100

    upgrade getposthome

--- src/controllers/Post/handlers/getPostsHome.ts
+++ src/controllers/Post/handlers/getPostsHome.ts
@@ -1,153 +1,145 @@
 import { RequestHandler } from "express";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import { integer } from "aws-sdk/clients/cloudfront";
 
-const getPostsHome : RequestHandler = (req, res, next) => {
-    const userEmail: string = res.locals.user.email;
+const getPostsHome: RequestHandler = (req, res, next) => {
+  const userEmail: string = res.locals.user.email;
 
-    const prisma = new PrismaClient()
+  const prisma = new PrismaClient();
 
-    async function main() {
-        const user = await prisma.user.findUnique({
-            where: {
-              email: userEmail,
+  async function main() {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    });
+    if (user) {
+      const userId = user.id;
+      const postsWithFrienships = await prisma.post.findMany({
+        where: {
+          user: {
+            AND: [
+              {
+                OR: [
+                  {
+                    fromFriendship: {
+                      some: {
+                        OR: [
+                          {
+                            toId: userId,
+                          },
+                          {
+                            fromId: userId,
+                          },
+                        ],
+                      },
+                    },
+                  },
+                  {
+                    toFrienship: {
+                      some: {
+                        OR: [
+                          {
+                            toId: userId,
+                          },
+                          {
+                            fromId: userId,
+                          },
+                        ],
+                      },
+                    },
+                  },
+                ],
+              },
+              {
+                id: {
+                  not: userId,
+                },
+              },
+            ],
+          },
+        },
+        include: {
+          postAttachments: {
+            select: {
+              s3Key: true,
+              type: true,
             },
-          })
-          if(user){
-            const userId = user.id;
-            const postsWithFrienships = await prisma.post.findMany({
-                where: {
-                  user: {
-                    AND: [
-                      {
+          },
+          user: true,
+          postLikes: true,
+        },
+      });
+      const lastPosts = await prisma.post.findMany({
+        where: {
+          user: {
+            NOT: [
+              {
+                OR: [
+                  {
+                    fromFriendship: {
+                      some: {
                         OR: [
                           {
-                            fromFriendship: {
-                              some: {
-                                OR: [
-                                  {
-                                    toId: userId,
-                                  },
-                                  {
-                                    fromId: userId,
-                                  },
-                                ],
-                              },
-                            },
+                            toId: userId,
                           },
                           {
-                            toFrienship: {
-                              some: {
-                                OR: [
-                                  {
-                                    toId: userId,
-                                  },
-                                  {
-                                    fromId: userId,
-                                  },
-                                ],
-                              },
-                            },
+                            fromId: userId,
                           },
                         ],
                       },
-                      {
-                        id: {
-                          not: userId,
-                        },
-                      },
-                    ],
-                  },
-                },
-                include: {
-                  _count: {
-                    select: {
-                      postLikes: true,
-                      postComments: true,
                     },
                   },
-                  postAttachments: {
-                    select: {
-                      s3Key: true,
-                      type: true,
-                    },
-                  },
-                },
-              });
-              const lastPosts = await prisma.post.findMany({
-                where: {
-                  user: {
-                    NOT: [
-                      {
+                  {
+                    toFrienship: {
+                      some: {
                         OR: [
                           {
-                            fromFriendship: {
-                              some: {
-                                OR: [
-                                  {
-                                    toId: userId,
-                                  },
-                                  {
-                                    fromId: userId,
-                                  },
-                                ],
-                              },
-                            },
+                            toId: userId,
                           },
                           {
-                            toFrienship: {
-                              some: {
-                                OR: [
-                                  {
-                                    toId: userId,
-                                  },
-                                  {
-                                    fromId: userId,
-                                  },
-                                ],
-                              },
-                            },
+                            fromId: userId,
                           },
                         ],
                       },
-                      {
-                        id: userId
-                      },
-                    ],
-                  },
-                },
-                include: {
-                  _count: {
-                    select: {
-                      postLikes: true,
-                      postComments: true,
                     },
                   },
-                  postAttachments: {
-                    select: {
-                      s3Key: true,
-                      type: true,
-                    },
-                  },
-                },
-              });
+                ],
+              },
+              {
+                id: userId,
+              },
+            ],
+          },
+        },
+        include: {
+          postLikes: true,
+          postAttachments: {
+            select: {
+              s3Key: true,
+              type: true,
+            },
+          },
+          user: true,
+        },
+      });
 
-              const allPosts = [];
-              allPosts.push(postsWithFrienships);
-              allPosts.push(lastPosts);
-            res.json(allPosts);
-        }
+      const allPosts = [];
+      allPosts.push(postsWithFrienships);
+      allPosts.push(lastPosts);
+      res.json(allPosts);
     }
+  }
 
-    main()
+  main()
     .then(async () => {
-        await prisma.$disconnect()
+      await prisma.$disconnect();
     })
     .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
-}
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+};
 
-export default getPostsHome
+export default getPostsHome;
